0633fe4df352babaeb173a338c4965084f62efdb
Renan Vasconcelos Rodrigues <72624229+R-Renan@users.noreply.github.com>
2024-08-06 22:34:52 -0300

    docs: form login

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,17 +1,32 @@
-// Add the correct import statement for useHistory
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const Login = () => {
-  const history = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
-    history("/dashboard");
-  };
+  const handleLogin = () => {};
 
   return (
     <div>
-      <p>Login</p>
-      <button onClick={handleLogin}>Login</button>
+      <form>
+        <label>
+          <span>Email</span>
+          <input value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+
+        <label>
+          <span>Password</span>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        <button type="button" onClick={handleLogin}>
+          Sing-In
+        </button>
+      </form>
     </div>
   );
 };
